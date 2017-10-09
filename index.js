7d3b67377a3c11208e5b88a9e9cc4a9eb2e631b8
Austin Kim <ask199416@gmail.com>
2017-10-09 02:53:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if (string===whisper(string)) {
     return "I can't hear you!"
   }
-  if (string==='Hello') {
+  if (string==='HELLO') {
     return "YES INDEED!"
   }
   if (string==="I love you, Grandma.") {
